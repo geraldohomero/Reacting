edf36108d17d81144b16968ba515edf90a3125c2
geraldohomero <geraldohomero@protonmail.com>
2024-02-28 20:11:08 -0300

    up

--- 1-import-export/src/App.js
+++ 1-import-export/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import apiKey from './Utils.js';
 // import { apiKey, abc } from './Utils.js';
 import * as Utils from './Utils.js'; // group all exports in an object
+import { createGreeting } from './Utils.js';
 // import { apiKey as key } from './Utils.js'; // rename the import with 'as' - just like alias
 //console.log(key); // uhauhopkasklpdaskl1234
 
